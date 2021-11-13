ad6575ff589e31265b540c7d0d315eae9a88148c
Aswin C <chandranaswin1997@gmail.com>
2021-11-13 16:45:02 +0530

    Provided the CLI for the tool with necessary options for the conversion

--- cli.py
+++ cli.py
@@ -2,6 +2,12 @@ import click
 import epub2sphinx
 
 @click.command()
-def convert():
-    click.echo("Hello world")
-    # Use epub2sphinx.convert_epub(...) here
+@click.option('-o','--output-directory',type=click.Path(),default='.',help="The name of the output directory where the ReST file will be generated.")
+@click.option('-t','--theme','sphinx_theme_name',default="Alabaster",prompt=True,help="The name of the sphinx theme. You can check for the available themes at https://sphinx-themes.org/#themes")
+@click.version_option()
+@click.argument('input_file',type=click.File("r"))
+def convert(output_directory,sphinx_theme_name,input_file):
+    ''' This tool helps you to convert your epub files into sphinx format for a better reading experience.'''
+    message = "Output directory : {} ; Theme : {} ; Input file : {}"
+    click.echo(message.format(output_directory,sphinx_theme_name,input_file))
+    epub2sphinx.convert_epub(input_file.name,output_directory,sphinx_theme_name)
