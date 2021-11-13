5c94f02cdfa8f022449531647b6d986b0ffd6d50
Aswin C <chandranaswin1997@gmail.com>
2021-11-13 19:22:34 +0530

    Changed the logic of directory existence check while creating directory structure

--- epub2sphinx/tests/unit_test.py
+++ epub2sphinx/tests/unit_test.py
@@ -4,6 +4,6 @@ import sys
 def unit_test(function_name):
     if "create_directory_structure" == function_name:
         print(function_name)
-        epub2sphinx.create_directory_structure(".")
+        epub2sphinx.create_directory_structure("testing",["source","build"])
 
 unit_test(sys.argv[1])
