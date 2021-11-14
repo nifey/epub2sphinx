7fb4ab98a9703d54bedc742b91b032b802fa22f1
Aswin C <chandranaswin1997@gmail.com>
2021-11-14 13:55:29 +0530

    Move the constants to a new file for easy maintanance.

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import click
 import epub2sphinx
+from epub2sphinx import constants
 import os
 
 def get_file_name(individual_file):
@@ -7,13 +8,15 @@ def get_file_name(individual_file):
         return individual_file.name
 
 @click.command()
-@click.option('-o','--output-directory',type=click.Path(),prompt=True,default=lambda : os.path.join(os.getcwd(),"output"),help="The name of the output directory where the ReST file will be generated. Kindly make sure that the given directory is not existing already.")
-@click.option('-t','--theme','sphinx_theme_name',default="alabaster",type=str,prompt=True,help="The name of the sphinx theme. You can check for the available themes at https://sphinx-themes.org/#themes")
+@click.option('-o','--output-directory',type=click.Path(),prompt=True,default=lambda : os.path.join(os.getcwd(),"output"),help=constants.cli_option_output_directory_help)
+@click.option('-t','--theme','sphinx_theme_name',default="alabaster",type=str,prompt=True,help=constants.cli_option_theme_help)
 @click.version_option()
 @click.argument('input_file',type=click.File("r"))
 def convert(output_directory,sphinx_theme_name,input_file):
-    ''' This tool helps you to convert your epub files into sphinx format for a better reading experience.
-        Kindly provide the epub file as the argument to this command.'''
+    '''\b
+        This tool helps you to convert your epub files into sphinx format for a better reading experience.
+        Kindly provide the epub file as the argument to this command.
+    '''
     input_files= [input_file]
     with click.progressbar(input_files,label="converting to sphinx",item_show_func=get_file_name) as bar:
         for individual_file in bar:
---
+++ epub2sphinx/constants.py
@@ -0,0 +1,6 @@
+cli_option_output_directory_help = """\b
+The name of the output directory where the ReST file will be generated.
+Kindly make sure that the given directory is not existing already."""
+cli_option_theme_help = """\b
+The name of the sphinx theme.You can check for the available themes at:
+<https://www.sphinx-doc.org/en/master/usage/theming.html#builtin-themes>"""
