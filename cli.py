2b65fb0c8fdaa0f165bb3555b90df9106f0965f2
Abdun Nihaal <abdun.nihaal@gmail.com>
2021-11-14 17:29:28 +0530

    Refactor --serve and Add option for --build

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import click
 import epub2sphinx
+import subprocess
 from epub2sphinx import constants
 import os
 
@@ -10,11 +11,12 @@ def get_file_name(individual_file):
 @click.command()
 @click.option('-o','--output-directory',type=click.Path(),prompt=True,default=lambda : os.path.join(os.getcwd(),"output"),help=constants.cli_option_output_directory_help)
 @click.option('-t','--theme','sphinx_theme_name',default="alabaster",type=str,prompt=True,help=constants.cli_option_theme_help)
-@click.version_option()
 @click.argument('input_file',type=click.File("r"))
-@click.option('-s', '--serve', prompt=True, type=bool, default='n', help="Serve the file on localhost")
+@click.option('-b', '--build', 'post_conversion', flag_value='build', help=constants.cli_option_build_help)
+@click.option('-s', '--serve', 'post_conversion', flag_value='serve', help=constants.cli_option_serve_help)
+@click.version_option()
 
-def convert(output_directory,sphinx_theme_name,input_file, serve):
+def convert(output_directory,sphinx_theme_name,input_file, post_conversion):
     '''\b
         This tool helps you to convert your epub files into sphinx format for a better reading experience.
         Kindly provide the epub file as the argument to this command.
@@ -22,5 +24,20 @@ def convert(output_directory,sphinx_theme_name,input_file, serve):
     input_files= [input_file]
     with click.progressbar(input_files,label="converting to sphinx",item_show_func=get_file_name) as bar:
         for individual_file in bar:
-            c = epub2sphinx.Converter(individual_file.name,output_directory,sphinx_theme_name.lower(), serve)
+            c = epub2sphinx.Converter(individual_file.name,output_directory,sphinx_theme_name.lower())
             c.convert()
+
+    if post_conversion:
+        # Build using Sphinx
+        os.chdir(output_directory)
+        build_exit_code = subprocess.call(["make html"], shell=True, stdout=subprocess.PIPE)
+        html_path = os.path.join('build', 'html')
+        if build_exit_code == 0 and os.path.isdir(html_path):
+            if post_conversion == 'serve':
+                # Serve on localhost
+                os.chdir(html_path)
+                subprocess.call(["python -m http.server --bind 127.0.0.1"], shell=True)
+            else:
+                click.echo("Build finished successfully")
+        else:
+            click.echo("Sphinx Build Failed: Something went wrong!")
--- epub2sphinx/constants.py
+++ epub2sphinx/constants.py
@@ -4,3 +4,11 @@ Kindly make sure that the given directory is not existing already."""
 cli_option_theme_help = """\b
 The name of the sphinx theme.You can check for the available themes at:
 <https://www.sphinx-doc.org/en/master/usage/theming.html#builtin-themes>"""
+cli_option_serve_help = """\b
+Build HTML using Sphinx and Serve the files on localhost.
+Sphinx has to be installed for this to work.
+"""
+cli_option_build_help = """\b
+Build HTML from the generated ReST files using Sphinx.
+Sphinx has to be installed for this to work.
+"""
