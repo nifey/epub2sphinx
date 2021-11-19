4cb5dae72f6df148fae689e89598db4e1e340770
Abdun Nihaal <abdun.nihaal@gmail.com>
2021-11-19 16:03:16 +0530

    Fix requirements and Update setup.py
    - Remove epub2sphinx from requirements
    - Add long_description, classifiers and issue tracker URL

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 
@@ -8,7 +10,20 @@ setup(
     version = '0.0.1',
     license = 'MIT',
     description = 'Tool to convert epub to ReST for Sphinx',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     url = 'https://github.com/nifey/epub2sphinx',
+    project_urls={
+        "Bug Tracker": "https://github.com/nifey/epub2sphinx/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Documentation :: Sphinx",
+    ],
     py_modules = ['cli', 'epub2sphinx'],
     packages = find_packages(),
     package_data={'epub2sphinx': ['templates/*']},
