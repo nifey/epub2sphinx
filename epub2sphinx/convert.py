1a4b2e20a9dc11f85762b67865d8c2687f62b06a
Abdun Nihaal <abdun.nihaal@gmail.com>
2021-11-24 23:26:44 +0530

    Refactor file extraction
    - Now NCX files are not extracted
    - Move css files to conf.py templates
    - Saving images, fonts, stylesheet is also done using ThreadPoolExecutor

--- epub2sphinx/convert.py
+++ epub2sphinx/convert.py
@@ -1,3 +1,4 @@
+import ebooklib
 import jinja2
 import os
 import shutil
@@ -11,6 +12,53 @@ from tqdm import tqdm
 
 templates_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)), "templates")
 
+def extract_file(item, source_directory, extract_style):
+    """Extracts the Image, CSS or Font file
+
+    :param item: EpubItem to extract
+    :type item: class:`ebook.epub.EpubItem`
+
+    :param source_directory: Source directory to extract the file to
+    :type source_directory: str
+
+    :param extract_style: Decides if CSS and Font items should be extracted
+    :type extract_style: bool
+    """
+    item_type = item.get_type()
+    if (item_type == ebooklib.ITEM_IMAGE or item_type == ebooklib.ITEM_COVER):
+        file_path = os.path.join(source_directory, item.file_name)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as ext_file:
+            ext_file.write(item.content)
+    elif (extract_style and (item_type == ebooklib.ITEM_STYLE or item_type == ebooklib.ITEM_FONT)):
+        file_path = os.path.join(source_directory, "_static", item.file_name)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as ext_file:
+            ext_file.write(item.content)
+        return item.file_name
+
+def generate_chapter(chapter_id, book, source_directory):
+    """Generate ReST for each chapter and write to output file
+
+    :param chapter_id: ID of the chapter
+    :type chapter_id: str
+
+    :param book: Book instance
+    :type book: class:`ebook.epub.EpubItem`
+
+    :param source_directory: Source directory to extract the file to
+    :type source_directory: str
+    """
+    chapter = Chapter(book, chapter_id[0])
+    # Create any parent directories as given in the filename
+    os.makedirs(os.path.dirname(os.path.join(source_directory, chapter.file)), exist_ok=True)
+    # Convert HTML to ReST
+    if not chapter.convert():
+        # Omit chapter from toctree
+        return None
+    chapter.write(source_directory)
+    return chapter.file
+
 class Converter:
 
     def __init__(self, file_name, output_directory, sphinx_theme_name,include_custom_css):
@@ -20,6 +68,7 @@ class Converter:
         self.static_files_directory = os.path.join(self.source_directory, '_static')
         self.theme = sphinx_theme_name
         self.include_custom_css = include_custom_css
+        self.css_files = None
 
     def convert(self):
         # Create output directory structure
@@ -27,69 +76,31 @@ class Converter:
         shutil.copytree(os.path.join(templates_directory,"makefiles"),
                         self.output_directory)
         os.makedirs(self.static_files_directory)
-        # Generate ReST file for each chapter in ebook
-        self.generate_rst()
-        # Extract images from epub
-        click.echo("Extracting images")
-        self.extract_images()
+
+        with ThreadPoolExecutor() as executor:
+            # Generate ReST file for each chapter in ebook
+            self.book.toctree = list(filter(None, tqdm(
+                executor.map(lambda x: generate_chapter(x, self.book, self.source_directory),
+                             self.book.epub.spine),
+                total=len(self.book.epub.spine),
+                desc="Generating ReST files",
+                colour='Blue')))
+            # Extract other files from epub
+            click.echo("Extracting images")
+            self.css_files = list(filter(None, executor.map(
+                lambda x: extract_file(x, self.source_directory, self.include_custom_css),
+                self.book.epub.get_items())))
+            print(self.css_files)
+
         # Render jinja templates
         click.echo("Generating conf.py and index.rst")
         jinja_env = Environment(
             loader=PackageLoader("epub2sphinx")
         )
         with open(os.path.join(self.source_directory,'conf.py'), 'x') as cf:
-            cf.write(jinja_env.get_template('conf.py').render(book=self.book, theme=self.theme))
+            if self.include_custom_css:
+                cf.write(jinja_env.get_template('conf.py').render(book=self.book, theme=self.theme, css_files=self.css_files))
+            else:
+                cf.write(jinja_env.get_template('conf.py').render(book=self.book, theme=self.theme))
         with open(os.path.join(self.source_directory,'index.rst'), 'x') as of:
             of.write(jinja_env.get_template('index.rst').render(book=self.book))
-
-    def generate_rst(self):
-        # Generate ReST file for each chapter in ebook
-        def generate_chapter(chapter_id):
-            chapter = Chapter(self.book, chapter_id[0])
-            # Create any parent directories as given in the filename
-            os.makedirs(os.path.dirname(os.path.join(self.source_directory, chapter.file)), exist_ok=True)
-            # Convert HTML to ReST
-            if not chapter.convert():
-                # Omit chapter from toctree
-                return None
-            chapter.write(self.source_directory)
-            return chapter.file
-
-        with ThreadPoolExecutor() as executor:
-            self.book.toctree = list(filter(None, tqdm(executor.map(generate_chapter, self.book.epub.spine), total=len(self.book.epub.spine), desc="Generating ReST files", colour='Blue')))
-
-    def extract_images(self):
-        # save all media, xml, font files for the current book to its source directory
-        files = self.book.epub.get_items()
-        html_css_files = []
-
-        for book_file in files:
-            if book_file.media_type == 'application/xhtml+xml':
-                continue
-            try:
-                file_name = os.path.split(book_file.file_name)[-1]
-
-                directories = (os.path.join(self.source_directory, book_file.file_name)).split(os.path.sep)
-                if len(directories) > 1:
-                    directory = os.path.sep.join(directories[:-1])
-                    if not os.path.exists(directory):
-                        os.makedirs(directory)
-
-                file_path = os.path.join(self.source_directory, book_file.file_name)
-                if book_file.media_type == 'text/css':
-                    if self.include_custom_css:
-                        # write css files to the _static directory
-                        file_path = os.path.join(self.static_files_directory, file_name)
-                        html_css_files.append(file_name)
-                    else:
-                        continue
-                # file.content is in bytes format
-                with open(file_path, 'wb') as ext_file:
-                    ext_file.write(book_file.content)
-            except Exception as error:
-                click.echo(error)
-
-        # include the css file paths to the conf file
-        if html_css_files:
-            with open(os.path.join(self.source_directory, 'conf.py'), 'a') as conf_file:
-                conf_file.write(f"html_css_files = {html_css_files}")
--- epub2sphinx/templates/conf.py
+++ epub2sphinx/templates/conf.py
@@ -55,3 +55,7 @@ html_theme = '{{ theme }}'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+{% if css_files %}
+html_css_files = {{ css_files }}
+{% endif %}
